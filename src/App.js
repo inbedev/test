12162cb80a3e74f89f8b63aa2994033e0b9c8723
atemengue <moafembe@gmail.com>
2022-10-01 08:21:35 +0100

    initial commit react-caytsi

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1>React JS WorkShop</h1>
-      <h2>Welcome tout a tous</h2>
+      <h1>ReactJS</h1>
+      <h2>Bonjour a tous!!</h2>
     </div>
   );
 }
